c7d1f375f9f5ddd29846112f3b8c04e6015818c8
Artem_Yefimenko <artem.efimenko72@gmail.com>
2023-04-30 20:20:53 +0300

    Update Program.cs

--- Hello_world/Hello_world/Program.cs
+++ Hello_world/Hello_world/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+int a = 10;
+short b = 11;
+long c = 12;
+double d = 13.1;
+
+Console.WriteLine(a + a);
+Console.WriteLine(b - a);
+Console.WriteLine(c * b);
+Console.WriteLine(d % a); 
+*/
+
+double x = 10;
+double y = 15;
+
+Console.WriteLine("First math example: " + (-6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15));
+
+Console.WriteLine("Second math example: " + (Math.Abs(x) * Math.Sin(x)));
+
+Console.WriteLine("Third math example: " + (2 * Math.PI * x));
+
+Console.WriteLine("Fourth math example: " + Math.Max(x, y));
+
