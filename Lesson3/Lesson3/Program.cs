4d972cbe92b4957f403f3c128a222200ac25518b
Artem_Yefimenko <artem.efimenko72@gmail.com>
2023-05-07 04:30:28 +0300

    Homework 3 added

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+int x = 10;
+int y = 5;
+*/
+int x = 0;
+int y = 0;
+int sum = 0;
+
+Console.Write("First number is: ");
+bool firstNumber = int.TryParse(Console.ReadLine(), out x);
+Console.Write("Second number is: ");
+bool secondNumber = int.TryParse(Console.ReadLine(), out y);
+
+if (!firstNumber || !secondNumber)
+    Console.WriteLine("Input is in incorrect format!");
+else
+{
+    if (x < y)
+    {
+        for (int i = x; i <= y; i++) sum += i;
+    }
+    else if (x >= y)
+    {
+        for (int i = y; i <= x; i++) sum += i;
+    }
+    Console.WriteLine("Sum of numbers between: " + sum);
+}
