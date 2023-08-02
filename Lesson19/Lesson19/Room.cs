c2626af66de2eb53a7681cd6630b6652d15523c3
Artem_Yefimenko <artem.efimenko72@gmail.com>
2023-08-02 19:01:58 +0300

    Homework 19 done

---
+++ Lesson19/Lesson19/Room.cs
@@ -0,0 +1,16 @@
+﻿public class Room
+{
+    public string Name { get; }
+    public Lesson Lesson { get; private set; }
+
+    public Room(string name)
+    {
+        Name = name;
+    }
+
+    public void SetLesson(Lesson lesson)
+    {
+        Lesson = lesson;
+    }
+}
+
---
+++ Lesson19/TestProject1/TeacherTests.cs
@@ -0,0 +1,17 @@
+﻿public class TeacherTests
+{
+    [Fact]
+    public void SetCathedra_WhenCathedraIsSet_ShouldSetCathedra()
+    {
+        // Arrange
+        Teacher teacher = new Teacher("John Doe");
+        Cathedra cathedra = new Cathedra("Computer Science");
+
+        // Act
+        teacher.SetCathedra(cathedra);
+
+        // Assert
+        Assert.Equal(cathedra, teacher.Cathedra);
+    }
+}
+
