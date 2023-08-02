0b09ccc77d96f5acd487139a70a94057c54733ec
Artem_Yefimenko <artem.efimenko72@gmail.com>
2023-08-02 18:34:32 +0300

    Revert "Homework18 done"
    This reverts commit 91c81622d3bdc13832057d5959282d58f0b83b5e.

--- Lesson18/Lesson18/Program.cs
+++ Lesson18/Lesson18/Program.cs
@@ -1,41 +1,82 @@
-﻿class Program
+﻿using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
 {
-    static void Main(string[] args)
+    class Program
     {
-        Cathedra cathedra = new Cathedra("Computer Science");
-        Group group = new Group("Group A", cathedra);
-        Lesson lesson = new Lesson("Math", new Teacher("John Doe"), new Room("Room 101"));
-        Student student = new Student("Jean Doe");
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter a location (e.g. city or address):");
+            string location = Console.ReadLine();
 
-        group.AddStudent(student);
-        Console.WriteLine($"Student {student.Name} added to {group.Name}");
+            string weatherApiKey = "YOUR_OPENWEATHERMAP_API_KEY";
+            string geolocationApiKey = "YOUR_GOOGLE_MAPS_GEOLOCATION_API_KEY";
 
-        group.RemoveStudent(student);
-        Console.WriteLine($"Student {student.Name} removed from {group.Name}");
+            try
+            {
+                string geolocationData = await GetGeolocationDataAsync(location, geolocationApiKey);
+                string latitude = ParseGeolocationData(geolocationData, "lat");
+                string longitude = ParseGeolocationData(geolocationData, "lng");
 
-        group.AddLesson(lesson);
-        Console.WriteLine($"Lesson {lesson.Name} added to {group.Name}");
+                if (latitude != null && longitude != null)
+                {
+                    string weatherData = await GetWeatherDataAsync(latitude, longitude, weatherApiKey);
+                    string weatherDescription = ParseWeatherData(weatherData, "description");
+                    string temperature = ParseWeatherData(weatherData, "temp");
+                    string humidity = ParseWeatherData(weatherData, "humidity");
+                    string windSpeed = ParseWeatherData(weatherData, "wind_speed");
 
-        group.RemoveLesson(lesson);
-        Console.WriteLine($"Lesson {lesson.Name} removed from {group.Name}");
+                    Console.WriteLine($"Weather in {location}:");
+                    Console.WriteLine($"Description: {weatherDescription}");
+                    Console.WriteLine($"Temperature: {temperature}°C");
+                    Console.WriteLine($"Humidity: {humidity}%");
+                    Console.WriteLine($"Wind Speed: {windSpeed} m/s");
+                }
+                else
+                {
+                    Console.WriteLine($"Unable to retrieve geolocation data for {location}.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
 
-        lesson.AddGroup(group);
-        Console.WriteLine($"Group {group.Name} added to {lesson.Name}");
+        static async Task<string> GetGeolocationDataAsync(string location, string apiKey)
+        {
+            string apiUrl = $"https://maps.googleapis.com/maps/api/geocode/json?address={location}&key={apiKey}";
 
-        lesson.RemoveGroup(group);
-        Console.WriteLine($"Group {group.Name} removed from {lesson.Name}");
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
 
-        cathedra.AddGroup(group);
-        Console.WriteLine($"Group {group.Name} added to {cathedra.Name}");
+        static async Task<string> GetWeatherDataAsync(string latitude, string longitude, string apiKey)
+        {
+            string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apiKey}";
 
-        cathedra.RemoveGroup(group);
-        Console.WriteLine($"Group {group.Name} removed from {cathedra.Name}");
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
 
-        Teacher teacher = new Teacher("Jane Smith");
-        cathedra.AddTeacher(teacher);
-        Console.WriteLine($"Teacher {teacher.Name} added to {cathedra.Name}");
+        static string ParseGeolocationData(string geolocationData, string field)
+        {
+            JObject data = JObject.Parse(geolocationData);
+            return data["results"]?.FirstOrDefault()?["geometry"]?[field]?.ToString();
+        }
 
-        cathedra.RemoveTeacher(teacher);
-        Console.WriteLine($"Teacher {teacher.Name} removed from {cathedra.Name}");
+        static string ParseWeatherData(string weatherData, string field)
+        {
+            JObject data = JObject.Parse(weatherData);
+            return data["main"]?[field]?.ToString();
+        }
     }
 }
